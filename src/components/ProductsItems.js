e540eaff62478d9e989bcb8662e40aac95f5296b
AMAL ALRASDAN <mariam.alrashdan97@gmail.com>
2023-06-04 06:19:52 +0300

    stayling

--- src/components/ProductsItems.js
+++ src/components/ProductsItems.js
@@ -17,9 +17,7 @@ const ProductsItems = ({ product }) => {
           <p className="mt-2 text-neutral-500">{product.price}</p>
           <p className="mt-2">{product.description}</p>
           <div className="flex justify-center font-bold mt-2">
-            <a href="" className="bg-neutral-300 py-2 px-5 rounded">
-              Order
-            </a>
+            <a className="bg-neutral-300 py-2 px-5 rounded">Order</a>
           </div>
         </div>
       </div>
