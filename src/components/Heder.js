a761e2efb920dab429732a50635ff2c0b0d0026c
AMAL ALRASDAN <mariam.alrashdan97@gmail.com>
2023-06-04 06:41:09 +0300

    update style2

--- src/components/Heder.js
+++ src/components/Heder.js
@@ -4,10 +4,10 @@ import image1 from "../assest/media/seaFood.jpeg";
 const Heder = () => {
   return (
     <div className="p-4 flex flex-col justify-center items-center">
-      <h1 className=" text-center text-3xl text-neutral-700 font-bold border-b border-cyan-900">
+      <h1 className=" text-center text-4xl text-neutral-700 font-bold border-b border-cyan-900">
         The Coasteal Catch
       </h1>
-      <h4 className="text-center text-xl text-neutral-950 font-semibold mt-8">
+      <h4 className="text-center text-l text-neutral-950 font-semibold mt-8">
         A seafood lover's paradise, offering a tempting selection of freshly
         caught delights from the ocean, expertly prepared with a creative twist
         that highlights the natural flavors of the sea.
